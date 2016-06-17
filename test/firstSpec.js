7bf13c08109da5ebdbac938362cc1ff5bf365937
pikachumetal <pikachu.metal@gmail.com>
2016-06-17 10:16:47 +0200

    testing 1

---
+++ test/firstSpec.js
@@ -0,0 +1,22 @@
+//Arrange
+
+//Act
+
+//Assert
+
+describe('JavaScript addition operator', function () {
+    it('adds two numbers together', function () {
+        expect(1 + 2).toEqual(3);
+    });
+    it('substract two numbers together', function () {
+        expect(1 - 2).toEqual(-1);
+    });
+});
+describe('JavaScript addition operator', function () {
+    it('adds two numbers together', function () {
+        expect(1 + 2).toEqual(2);
+    });
+    it('substract two numbers together', function () {
+        expect(1 - 2).toEqual(-1);
+    });
+});
