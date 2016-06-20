ccf8db427bb44049cc76eb382670b635913f5eb7
pikachumetal <pikachu.metal@gmail.com>
2016-06-20 13:42:38 +0200

    commit message

--- test/protractor/spec.js
+++ test/protractor/spec.js
@@ -1,7 +1,36 @@
 describe('Protractor Demo App', function() {
-  it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
 
-    expect(browser.getTitle()).toEqual('Super Calculator');
+  function add(a, b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
+
+  beforeEach(function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
   });
+
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
+
+    expect(history.count()).toEqual(2);
+
+    add(5, 6);
+
+    expect(history.count()).toEqual(3); // This is wrong!
+  });
+
+  it('should have a history', function() {
+      add(1, 2);
+      add(3, 4);
+
+      expect(history.last().getText()).toContain('1 + 2');
+      expect(history.first().getText()).toContain('3 + 4'); // This is wrong!
+    });
 });
